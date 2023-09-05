d827562e9619ca249155d4f949ea21c59ce33025
MarEduardo <136724561+MarEduardo@users.noreply.github.com>
2023-09-05 09:47:01 -0300

    Add files via upload

---
+++ PrjAgenda2023/PrjAgenda2023/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: POLIMIG
+ * Data: 05/09/2023
+ * Hora: 09:12
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PrjAgenda2023
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class FRMPRINCIPAL : Form
+	{
+		public FRMPRINCIPAL()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
